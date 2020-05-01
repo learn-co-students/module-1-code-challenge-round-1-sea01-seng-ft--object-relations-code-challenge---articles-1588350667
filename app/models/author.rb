9611119fb6dfad18f3f84510f209baca35f7ffcb
matthewsedlacek <sedlacek1991@gmail.com>
2020-05-01 12:07:31 -0700

    hope this works

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,34 @@
 class Author
-  attr_accessor :name
+  attr_reader :name
 
+
+  @@all = []
 
   def initialize(name)
     @name = name
-  
+    @@all <<self
   end
 
+  def self.all
+    @@all
+  end
+
+  def articles
+    ##double check on this one
+    Article.all.select {|article|article.author == self}
+  end
+
+  def magazines
+    articles_name = Article.all.select {|article|article.author == self}
+      articles_name.map {|article|article.magazine}.uniq
+  end
+
+  def topic_areas
+    magazines.map {|art|art.category}.uniq
+  end
+
+  def add_article(magazine, title)
+    Article.new(self,magazine,title)
+  end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -10,9 +10,56 @@ end
 
 
 
+#Author
+matthew = Author.new("Matthew")
+matthew2 = Author.new("Matthew")
+edward = Author.new("Edward")
+joseph = Author.new("Joseph")
+
+Author.all
+matthew.name
+joseph.name
+# confirmed that it's only a reader. won't update
+#matthew2.name = "Matt"
+matthew.articles
+# matthew.magazines
 
 
 
+
+#Magazine
+# (name, category)
+vogue = Magazine.new("Vogue","Fashion")
+sports = Magazine.new("Sports Illustrated", "Sports")
+vibe = Magazine.new("Vibe Baskeball", "Sports")
+vogue2 = Magazine.new("Vogue","Sports")
+
+Magazine.all
+vogue.name
+vogue.category
+vibe.category
+Magazine.find_by_name("Vouge")
+
+# def initialize(author, magazine, title)
+#article
+a_1 = Article.new(matthew, vogue, "Dope Shoes" )
+a_2 = Article.new(matthew2, vogue, "leisure suits")
+a_3 = Article.new(matthew, vogue, "Tennis Shirts" )
+a_4 = Article.new(matthew, sports, "Top 10 Tennis Players")
+a_5 = Article.new(matthew, vibe, "Best Dunks of All Time" )
+a_6 = Article.new(edward, vogue, "Yoga Outfits")
+a_7 = Article.new(joseph, vibe, "Is Gonzaga due for a title" )
+a_8 = Article.new(matthew, vogue, "Dope" )
+a_1.author
+a_1.magazine
+a_7.author
+a_7.magazine
+
+Article.all
+vibe.article_titles
+
+#association
+matthew.add_article(vibe,"How to hit a backhand")
 
 
 ### DO NOT REMOVE THIS
