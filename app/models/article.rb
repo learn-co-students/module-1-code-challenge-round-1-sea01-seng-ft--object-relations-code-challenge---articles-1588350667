3438d7f87cee38aa38a2abd3b7939309e8a1b7ef
klarissasmith <ksmith_email@yahoo.com>
2020-05-01 11:16:56 -0700

    Finished

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,23 @@
 class Article
+    attr_accessor
+    attr_writer
+    attr_reader :title, :author, :magazine
+    @@all = []
+        
+        def initialize(author, magazine, title)
+            @author = author
+            @magazine = magazine
+            @title = title
+            save
+        end
 
+        def save
+            @@all << self
+        end
 
+        def self.all
+            @@all
+        end
+
+    
 end
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,10 +1,30 @@
+require 'pry'
 class Author
-  attr_accessor :name
+  attr_accessor 
+  attr_reader :name
+  attr_writer :articles
 
 
   def initialize(name)
     @name = name
-  
+    @articles = []
+    @magazines = []
+  end
+
+  def add_article_to_articles(article)
+    if article.author.name == self.name
+      @articles << article
+    end
+  end
+
+  def articles
+    self.articles = @articles
+  end
+
+  def add_magazine(magazine)
+    magazines
+      @magazines << magazine
+    end
   end
 
 
