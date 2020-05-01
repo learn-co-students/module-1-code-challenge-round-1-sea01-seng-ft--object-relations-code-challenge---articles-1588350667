c544f33be211e86dfbe788d2b746d5e8d9e68fce
NicoleQiu99 <nicolepeng99@gmail.com>
2020-05-01 11:43:13 -0700

    test

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,6 @@
 class Author
-  attr_accessor :name
+  #attr_accessor :name
+  attr_reader :name
 
 
   def initialize(name)
@@ -7,5 +8,28 @@ class Author
   
   end
 
+  def articles
+     Article.all.select {|article| article.author == self}
+  end
+
+  def magazines
+     article = self.articles.select{|art| art.author == self}.uniq
+     article.map do |a|
+        a.magazine
+     end
+  end
+
+  def add_article(magazine, title)
+     article = Article.new(self, magazine, title)
+     article.author = self
+     article
+  end
+
+  def topic_areas
+    article = self.articles.select{|art| art.author == self}.uniq
+    article.map do |a|
+       a.title
+    end
+  end
 
 end
