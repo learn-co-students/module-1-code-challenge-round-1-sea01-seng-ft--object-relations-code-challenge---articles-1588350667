dc40a79350a3cc3e46619f9a00e9430ce568c86b
jassyfrassy <jsmnramirez25@gmail.com>
2020-05-01 11:21:50 -0700

    JC's test

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,22 @@
 class Article
+    attr_reader :author, :magazine, :title
+   # attr_writer 
+
+    @@all = []
+
+    def initialize(author, magazine, title)
+        @author = author
+        @magazine = magazine
+        @title = title
+    
+        @@all << self
+    end
 
 
+    def self.all
+        @@all
+      end
 end
+
+
+#single source of truth because an author has many articles and a magazine has many articles
